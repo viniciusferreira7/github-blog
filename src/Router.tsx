f3582e5a9ca694dcdf7ce8f35176726c77daa987
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-01-07 14:56:47 -0300

    feat(src): estruturando o pastas e o sistema de rotas

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
