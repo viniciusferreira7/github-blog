0432796f500a678c04ef0ef1e7abcf6e08318a22
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-01-18 11:18:50 -0300

    feat(searchpost): exibição da quantidade de posts na tela

--- src/pages/Home/components/SearchPost/index.tsx
+++ src/pages/Home/components/SearchPost/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react'
+import { PostContext } from '../../../../context/PostContext'
 import { SearchPostsContainer } from './styles'
 
 export function SearchPosts() {
+  const { posts } = useContext(PostContext)
+
   return (
     <SearchPostsContainer>
       <h2>Publicações</h2>
-      <span>6 publicações</span>
+      <span>{posts?.length} publicações</span>
       <input type="text" placeholder="Buscar conteúdo" required />
     </SearchPostsContainer>
   )
